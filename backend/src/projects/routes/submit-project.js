f6a0a2152a4b43b022b4ce9f434b20a825dd6016
Jianghao Wu <wujianghao0706@gmail.com>
2023-12-02 07:08:00 -0500

    Update submit-project.js

--- backend/src/projects/routes/submit-project.js
+++ backend/src/projects/routes/submit-project.js
@@ -1,35 +1,50 @@
-const express = require('express');
-const { ProcessStatus } = require('../../common/process-status');
-const { requireAuth } = require('../../common/middleware/require-auth');
-const { BadRequestError } = require('../../common/errors/bad-request-error');
-const mongoose = require('mongoose');
-const { Project } = require('../models/project');
+const express = require("express");
+const { ProcessStatus } = require("../../common/process-status");
+const { requireAuth } = require("../../common/middleware/require-auth");
+const { BadRequestError } = require("../../common/errors/bad-request-error");
+const mongoose = require("mongoose");
+const { Project } = require("../models/project");
+const { web3, abi } = require("../../common/web3-lib");
 
 const router = express.Router();
 
-router.put('/api/projects/submit/:projectId', requireAuth, async (req, res) => {
-    const { output_file } = req.body;
+router.put("/api/projects/submit/:projectId", requireAuth, async (req, res) => {
+  const { output_file } = req.body;
 
-    if (!req.params.projectId || !mongoose.isValidObjectId(req.params.projectId)) {
-        throw new BadRequestError('Invalid Project Id');
-    }
-    const project = await Project.findById(req.params.projectId);
+  if (
+    !req.params.projectId ||
+    !mongoose.isValidObjectId(req.params.projectId)
+  ) {
+    throw new BadRequestError("Invalid Project Id");
+  }
+  const project = await Project.findById(req.params.projectId);
 
-    if (!project) {
-        throw new BadRequestError('Invalid Project Id');
-    }
+  if (!project) {
+    throw new BadRequestError("Invalid Project Id");
+  }
 
-    if (req.currentUser.id === project.creatorId || req.currentUser.id === project.verifierId) {
-        throw new BadRequestError('You cannot submit the work');
-    }
+  if (
+    req.currentUser.id === project.creatorId ||
+    req.currentUser.id === project.verifierId
+  ) {
+    throw new BadRequestError("You cannot submit the work");
+  }
 
-    project.freelancerId = req.currentUser.id;
-    project.output_file = output_file;
-    project.set('processStatus', ProcessStatus.Submitted);
+  const contract = new web3.eth.Contract(abi, project.contractAddress);
 
-    await project.save();
+  project.freelancerId = req.currentUser.id;
+  await contract.methods.submitWork(output_file).send({
+    from: req.currentUser.address,
+    gas: 999999,
+    type: "0x1",
+  });
+  console.log("Job submission successful");
+  project.output_file = output_file;
+  project.set("processStatus", ProcessStatus.Submitted);
 
-    res.send({ project });
+  await project.save();
+
+  res.send({ project });
 });
 
 module.exports = { submitProjectRouter: router };
