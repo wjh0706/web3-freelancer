ba66307b50ad49d75cded4bdaaeaba3165dfcbd1
Jianghao Wu <wujianghao0706@gmail.com>
2023-12-03 22:50:06 -0500

    fix merge issues

--- Submissions/thirdPartycode.py
+++
@@ -1,7 +0,0 @@
-import poster_code
-import json
-
-final_code = poster_code.code_returner()
-# Write the verification code to a JSON file
-with open('verification_code.json', 'w') as json_file:
-    json.dump({'verification_code': final_code}, json_file)
